b7a235a61f16d7eb4f3f24cfb7ccddade37d9b13
s_sergeev <serzhserg98@gmail.com>
2025-05-12 19:47:35 +0000

    Initial commit with Spring Boot VPN project

---
+++ src/main/java/com/vpnservice/VpnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vpnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpnServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vpnservice/VpnCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
